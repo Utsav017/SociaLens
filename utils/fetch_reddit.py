cabeaaae187850e05d3fafaa9110ed094e9dd2f6
Shivansh Gupta <137164625+Utsav017@users.noreply.github.com>
2025-08-23 20:58:40 +0530

    Update fetch_reddit.py

--- utils/fetch_reddit.py
+++ utils/fetch_reddit.py
@@ -1,13 +1,18 @@
 import praw
+import streamlit as st
 import pandas as pd
 
 def fetch_reddit(subreddit, limit=50):
     reddit = praw.Reddit(
-        client_id="YOUR_CLIENT_ID",
-        client_secret="YOUR_CLIENT_SECRET",
-        user_agent="sociaLens_app"
+        client_id=st.secrets["REDDIT_CLIENT_ID"],
+        client_secret=st.secrets["REDDIT_CLIENT_SECRET"],
+        user_agent=st.secrets["REDDIT_USER_AGENT"],
     )
+
     posts = []
     for submission in reddit.subreddit(subreddit).hot(limit=limit):
-        posts.append({"date": submission.created_utc, "text": submission.title + " " + submission.selftext})
+        posts.append({
+            "date": submission.created_utc,
+            "text": submission.title + " " + submission.selftext
+        })
     return pd.DataFrame(posts)
